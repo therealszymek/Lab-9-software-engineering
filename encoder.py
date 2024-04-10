6faf33da57275d9d69bc77c5c04d4bb625989765
Szymon Sierzega <szymonsierzega@gmail.com>
2024-04-09 21:08:30 -0400

    second commit

--- encoder.py
+++ encoder.py
@@ -1,4 +1,22 @@
 # Szymon Sierzega 04/09/2024
 
-def encode():
-    return None
+def encode(password):
+
+    new_password = ""
+
+    for char in password:
+        char = int(char)
+        char += 3
+
+        if char >= 10:
+            char = 0
+
+        char = str(char)
+        new_password += char
+
+    return new_password
+
+
+print(encode("12345555"))
+
+
