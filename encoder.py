b662d44dc7c6c85bf125469fa5219ef78b66b9e8
okar0 <oskar.dolega@gmail.com>
2024-04-09 22:17:38 -0400

    Oskar's commit

--- encoder.py
+++ encoder.py
@@ -15,14 +15,24 @@ def encode(password):
 
     return new_password
 
+def decode(encoded_password):
+    new_password = ""
+    for char in str(encoded_password):
+        char = int(char)
+        char -= 3
+        if char < 0:
+            char += 10
+        new_password += str(char)
+    return new_password
+
 
 def main():
+    encoded_password = " "
     while True:
-        encoded_password = ""
 
         print("Menu "
               "\n-------------"
-              "\n 1. Encode"
+              "\n1. Encode"
               "\n2. Decode"
               "\n3. Quit"
               "\n")
@@ -30,12 +40,12 @@ def main():
         user_input = input("Please enter an option: ")
 
         if user_input == "1":
-            password_input = input("Please enter your password to encode: ")
+            password_input = str(input("Please enter your password to encode: "))
             encoded_password = encode(password_input)
             print("Your password has been encoded and stored!")
 
         elif user_input == "2":
-            decoded_password = None  # decode(encoded_password)
+            decoded_password = decode(encoded_password)  # decode(encoded_password)
             print(f'The encoded password is {encoded_password}, and the original password is {decoded_password}')
 
         elif user_input == '3':
